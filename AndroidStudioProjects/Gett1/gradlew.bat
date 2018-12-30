177e6abc3d221c434fdff6c2c016b38c8eaf8c40
Esther <estherika9@gmail.com>
2018-12-31 00:31:12 +0200

    first upload

---
+++ AndroidStudioProjects/Gett1/gradlew.bat
@@ -0,0 +1,46 @@
+<application>
+  <component name="ActionsCollector">
+    <counts>
+      <entry action="$Copy" count="3" />
+      <entry action="$Cut" count="3" />
+      <entry action="$Delete" count="10" />
+      <entry action="$Paste" count="4" />
+      <entry action="$Redo" count="3" />
+      <entry action="$SelectAll" count="13" />
+      <entry action="$Undo" count="118" />
+      <entry action="Android.RunAndroidAvdManager" count="14" />
+      <entry action="Android.RunAndroidSdkManager" count="12" />
+      <entry action="AndroidConnectDebuggerAction" count="1" />
+      <entry action="ChangesView.NewChangeList" count="1" />
+      <entry action="CheckForUpdate" count="1" />
+      <entry action="CheckinFiles" count="2" />
+      <entry action="CheckinProject" count="12" />
+      <entry action="CleanGradleProject" count="1" />
+      <entry action="CloseProject" count="4" />
+      <entry action="CodeCompletion" count="46" />
+      <entry action="CollapseSelection" count="6" />
+      <entry action="CommentByLineComment" count="32" />
+      <entry action="CompareTwoFiles" count="1" />
+      <entry action="ContextHelp" count="1" />
+      <entry action="Debug" count="17" />
+      <entry action="DeveloperServices.Firebase" count="6" />
+      <entry action="DialogOkAction Enter" count="68" />
+      <entry action="EditorBackSpace" count="808" />
+      <entry action="EditorChooseLookupItem" count="265" />
+      <entry action="EditorCopy" count="87" />
+      <entry action="EditorCut" count="18" />
+      <entry action="EditorDelete" count="264" />
+      <entry action="EditorDown" count="2198" />
+      <entry action="EditorDownWithSelection" count="1" />
+      <entry action="EditorEnter" count="272" />
+      <entry action="EditorLeft" count="529" />
+      <entry action="EditorLeftWithSelection" count="13" />
+      <entry action="EditorLineEnd" count="19" />
+      <entry action="EditorLineEndWithSelection" count="17" />
+      <entry action="EditorLineStart" count="5" />
+      <entry action="EditorLineStartWithSelection" count="8" />
+      <entry action="EditorPageDownWithSelection" count="24" />
+      <entry action="EditorPageUp" count="24" />
+      <entry action="EditorPaste" count="160" />
+      <entry action="EditorRight" count="1114" />
+      <ent
